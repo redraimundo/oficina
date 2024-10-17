0cabf9d67371ac6125cc2b559e6e18f3dcc4e8df
redraimundo <eduardoraimundoferreirasilva@gmail.com>
2024-10-17 14:16:04 -0300

    aaoa

--- src/config/db.js
+++ src/config/db.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose'
 
 const connectDb = async() => {
     mongoose
-        .connect(`${process.env.URI}/prova`)
+        .connect(`${process.env.URI}`)
         .then(() => console.log('Conectado ao MongoDB'))
         .catch((error) => console.log(error))
 }
