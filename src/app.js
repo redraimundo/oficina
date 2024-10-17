9f9ab2b8c9ff680c843de709734de0cfb0f4e358
redraimundo <eduardoraimundoferreirasilva@gmail.com>
2024-10-17 14:09:39 -0300

    aaoa

--- src/app.js
+++ src/app.js
@@ -15,4 +15,4 @@ app.use('/workshop', workshopRouter)
 app.use('/vehicle', vehicleRouter)
 app.use('/maintenance', maintenanceRouter)
 
-app.listen(3000, () => console.log('Servidor rodando na porta 3000'))
+app.listen(process.env.PORT, () => console.log('Servidor rodando na porta 3000'))
